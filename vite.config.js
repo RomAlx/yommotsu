7d36e0bc0b4173b131ccf93b301efdace8fad37c
RomAlx <aaromanovsky@ya.ru>
2023-08-12 12:20:22 +0300

    fixs

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: '185.20.226.72',
-        },
-    },
     plugins: [
         vue(
             {
