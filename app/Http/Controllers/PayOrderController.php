fdd934852f904715ed6561ce1e9fd6a685a6d80b
RomAlx <aaromanovsky@ya.ru>
2023-08-12 03:04:18 +0300

    docker fixs

---
+++ app/Http/Controllers/PayOrderController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exports\PayOrdersExport;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+
+class PayOrderController extends Controller
+{
+    public function export(Request $request)
+    {
+        
+    }
+}
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component. Hello {{ username }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="'ts'">
+    import {defineComponent} from 'vue';
+    
+    export default defineComponent({
+        components: {},
+        setup() {
+            return {
+                username: 'Михаил'
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    });
+</script>
