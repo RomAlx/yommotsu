ad377b4b8f4849b5018ac58b04ad82e11c2c52ab
RomAlx <aaromanovsky@ya.ru>
2023-09-19 12:04:57 +0300

    fixs

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,20 @@ const host = 'yommotsu.com';
 
 export default defineConfig({
     //dev
-    server: {
-        hmr: {
-            host: `localhost`
-        }
-    },
-    //prod
     //server: {
-    //    host,
-    //    hmr: { host },
-    //    https: {
-    //        key: fs.readFileSync(`/etc/ssl/yommotsu.key`),
-    //        cert: fs.readFileSync(`/etc/ssl/yommotsu.crt`),
+    // hmr: {
+    //        host: `localhost`
     //    }
     //},
+    //prod
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`/etc/ssl/yommotsu.key`),
+            cert: fs.readFileSync(`/etc/ssl/yommotsu.crt`),
+        }
+    },
     plugins: [
         vue({
             template: {
