5128aa4cdb51cfb1525479024e048404fac13ee9
RomAlx <aaromanovsky@ya.ru>
2023-11-01 00:00:01 +0300

    fix export

--- app/Exports/PayOrdersExport.php
+++ app/Exports/PayOrdersExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\PayOrder;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -32,17 +33,27 @@ class PayOrdersExport implements FromCollection, WithHeadings, WithStyles, WithC
     public function columnFormats(): array
     {
         return [
-            'F' => NumberFormat::FORMAT_DATE_DATETIME,
+            'H' => NumberFormat::FORMAT_DATE_DATETIME,
         ];
     }
 
     public function map($payOrderExport): array
     {
+        if(!is_null($payOrderExport->bank)) {
+            
+            $bank = json_decode($payOrderExport->bank)->bank;
+            $name = json_decode($payOrderExport->bank)->name;
+        } else {
+            $bank = 'no info';
+            $name = 'no info';
+        }
         return [
             $payOrderExport->id,
             $payOrderExport->project_name,
             $payOrderExport->order_id,
             $payOrderExport->amount,
+            $bank,
+            $name,
             $payOrderExport->status,
             Date::dateTimeToExcel($payOrderExport->created_at),
         ];
@@ -50,14 +61,14 @@ class PayOrdersExport implements FromCollection, WithHeadings, WithStyles, WithC
 
 
     public function headings(): array {
-        return ['ID', 'Название проекта', 'ID заказа', 'Сумма', 'Статус', 'Дата создания'];
+        return ['ID', 'Название проекта', 'ID заказа', 'Сумма', 'Банк', 'Владелец', 'Статус', 'Дата создания'];
     }
 
     public function styles(Worksheet $sheet)
     {
         // Установить центрирование содержимого
-        $sheet->getStyle('A1:G1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-        $sheet->getStyle('A2:G'.$sheet->getHighestRow())->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('A1:H1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('A2:H'.$sheet->getHighestRow())->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
 
         // Задать ширину столбцов
         $sheet->getColumnDimension('A')->setWidth(5);
@@ -66,15 +77,17 @@ class PayOrdersExport implements FromCollection, WithHeadings, WithStyles, WithC
         $sheet->getColumnDimension('D')->setWidth(20);
         $sheet->getColumnDimension('E')->setWidth(20);
         $sheet->getColumnDimension('F')->setWidth(20);
+        $sheet->getColumnDimension('G')->setWidth(15);
+        $sheet->getColumnDimension('H')->setWidth(20);
 
         // Установить красный фон и белый текст для заголовков
-        $sheet->getStyle('A1:F1')
+        $sheet->getStyle('A1:H1')
             ->getFill()
             ->setFillType(Fill::FILL_SOLID)
             ->getStartColor()
             ->setARGB('FF0000'); // красный
 
-        $sheet->getStyle('A1:F1')
+        $sheet->getStyle('A1:H1')
             ->getFont()
             ->getColor()
             ->setARGB(Color::COLOR_WHITE); // белый
